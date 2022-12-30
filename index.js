23386f77d8ea634dec2a98cf049768927a8764be
Christian Albert <calbert1209@gmail.com>
2022-12-30 23:17:17 +0900

    add assembly script version

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const h1 = document.createElement('h1');
-h1.textContent = "hello from JS!";
-document.body.appendChild(h1);
-
 async function loadWasm(filename) {
   const response = await fetch(filename);
   const arrayBuffer = await response.arrayBuffer();
@@ -25,18 +21,57 @@ function measure(label, cb) {
 
   const end = performance.now();
 
-  console.log('%s : %d, result: %s', label, end - start, result.toString());
+  // console.log('%s : %d, result: %s', label, end - start, result.toString());
+  return {
+    label,
+    result,
+    time: end - start,
+  }
+}
+
+function createCell(textContent) {
+  const td = document.createElement('td');
+  td.textContent = textContent;
+  return td;
+}
+
+function createRow(result) {
+  const tr = document.createElement('tr');
+  const labelCell = createCell(result.label);
+  tr.appendChild(labelCell);
+
+  const resultCell  = createCell(result.result);
+  tr.appendChild(resultCell);
+
+  const timeCell  = createCell(result.time.toFixed(3));
+  tr.appendChild(timeCell);
+
+  return tr;
 }
 
 (async function main() {
-  const instance = await loadWasm('fibonacci.wasm');
-  myMathModule['fibCpp'] = instance.exports._Z3fibi;
+  const cpp = await loadWasm('output/fibonacci.wasm');
+  myMathModule['fibCpp'] = cpp.exports._Z3fibi;
+  const assemblyScript = await loadWasm('output/fib-as.wasm');
+  myMathModule['fibAs'] = assemblyScript.exports.fib;
 
-  console.log('%ctrying via WASM...','color: blue');
-  measure('via WASM', () => myMathModule.fibCpp(40));
+  const n = 40;
+  const tbody = document.getElementById('tbody');
+  console.log('%ctrying via CPP WASM...','color: blue');
+  const cppResult = measure('C++', () => myMathModule.fibCpp(n));
+  const cppRow = createRow(cppResult);
+  tbody.appendChild(cppRow);
+
+  console.log('%ctrying via AS WASM...','color: blue');
+  const asResult = measure('via AssemblyScript', () => myMathModule.fibAs(n));
+  const asRow = createRow(asResult);
+  tbody.appendChild(asRow);
+
   console.log('%ctrying via JS. This will take a bit of time...','color: blue');
-  measure('via JS', () => myMathModule.fibJs(40));
-  // measure('via JS', () => console.log('fib via js'));
-  // measure('via WASM', () => console.log('fib via wasm'));
-  
+  const jsResult = measure('via JavaScript', () => myMathModule.fibJs(n));
+  const jsRow = createRow(jsResult);
+  tbody.appendChild(jsRow);
+
+  const rowData = [cppResult, asResult, jsResult];
+  console.table(rowData);
 })();
---
+++ output/fib-as.wat
@@ -0,0 +1,30 @@
+(module
+ (type $i32_=>_i32 (func_subtype (param i32) (result i32) func))
+ (global $~lib/memory/__data_end i32 (i32.const 8))
+ (global $~lib/memory/__stack_pointer (mut i32) (i32.const 32776))
+ (global $~lib/memory/__heap_base i32 (i32.const 32776))
+ (memory $0 0)
+ (table $0 1 1 funcref)
+ (elem $0 (i32.const 1))
+ (export "fib" (func $fibonacci/fib))
+ (export "memory" (memory $0))
+ (func $fibonacci/fib (type $i32_=>_i32) (param $0 i32) (result i32)
+  local.get $0
+  i32.const 1
+  i32.le_s
+  if
+   local.get $0
+   return
+  end
+  local.get $0
+  i32.const 1
+  i32.sub
+  call $fibonacci/fib
+  local.get $0
+  i32.const 2
+  i32.sub
+  call $fibonacci/fib
+  i32.add
+  return
+ )
+)
